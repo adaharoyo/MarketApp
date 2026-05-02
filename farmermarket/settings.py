01a2766b8ed64b0436dbf3e6a7ace42095d5ec0f
Abdallah Lutaaya <bdllhltydrss@gmail.com>
2026-05-02 14:50:11 +0300

    Update STORAGES dict for Django 5.x Cloudinary compatibility

--- farmermarket/settings.py
+++ farmermarket/settings.py
@@ -64,11 +64,25 @@ if CLOUDINARY_URL or (CLOUDINARY_STORAGE['CLOUD_NAME'] and CLOUDINARY_STORAGE['A
             api_secret=CLOUDINARY_STORAGE['API_SECRET'],
             secure=True
         )
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STORAGES = {
+        "default": {
+            "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
 else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
 
 # Application definition
 
@@ -164,7 +178,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
