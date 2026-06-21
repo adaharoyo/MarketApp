33f56c619f05368ea4b886cc07d1a8759a2080c1
adaharoyo <158147987+adaharoyo@users.noreply.github.com>
2026-06-21 16:06:27 +0300

    Add ready method to FarmersmarketConfig

--- farmersmarket/apps.py
+++ farmersmarket/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FarmersmarketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'farmersmarket'
+
+ def ready(self):
+        import farmersmarket.signals
